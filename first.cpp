38b4455d17061eee3ab6ad4a504f22c03a6f0798
Ayush Kumar <ayushkj043@gmail.com>
2025-01-21 12:42:32 +0530

    the-one

---
+++ first.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+/*int main()
+{
+    int i=20,j=30;
+    cout<<"Hello"<<i<< endl <<j<<"world";
+    return 0;
+}*/
+
+/*int main()
+{
+    int i,j;
+    cin>>i>>j;
+    cout<<"The value of i is:"<<i<< endl <<"and the value of j is:"<<j;
+    return 0;
+}*/
+
+int main()
+{
+    string firstname,lastname;
+    cout << "Enter your first name : ";
+    cin >> firstname;
+    cout << "Enter your last name : ";
+    cin >> lastname;
+    cout << "The name you entered is : " << firstname << " " << lastname ;
+    return 0;
+}
+
