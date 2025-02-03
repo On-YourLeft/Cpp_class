245cdce8b50a55ce0265deaeab69ea72af48eb65
Ayush Kumar <ayushkj043@gmail.com>
2025-02-03 09:34:10 +0530

    the-eight

---
+++ class/one.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int row,col;
+    cout<<"Enter the rows and columns that you want to test in (r c) : ";
+    cin>>row>>col;
+    int arr2d[row][col];
+    for (int r=0;r<row;r++)
+    {
+        for (int c=0;c<col;c++)
+        {
+            cout<<"Enter the value [ "<<r<<" "<<c<<" ] :";
+            cin>> arr2d[r][c] ;
+        }
+    }
+    for (int r=0;r<row;r++)
+    {
+        for (int c=0;c<col;c++)
+        {
+            cout<<arr2d[r][c] <<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
