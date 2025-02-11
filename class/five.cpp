0098e0ec4b786ccd1e0274fc0d7cce7ac73dc45e
Ayush Kumar <ayushkj043@gmail.com>
2025-02-11 12:37:33 +0530

    the-ten

--- class/five.cpp
+++ class/five.cpp
@@ -2,7 +2,41 @@
 
 using namespace std;
 
+/*void greet(string fname,string mname=" ",string lname="")
+{
+    cout<<"Hello "<<fname<<" "<<mname<<" "<<lname<<endl;
+}
+
 int main()
 {
-    
+    greet("ayush");
+    greet("arbab","","Ansari");
+}*/
+
+/*int addon(int num1,int num2,int num3=0,int num4=0)
+{
+    return num1+num2+num3+num4;
+}
+
+int main()
+{
+    cout<<addon(1,2);
+}*/
+
+//Polymorphism
+
+int area(int num)
+{
+    return num*num;
+}
+
+float area(float num1,float num2)
+{
+    return (0.5*num1*num2);
+}
+
+int main()
+{
+    cout<<area(2)<<endl;
+    cout<<area(4.2,6.5)<<endl;
 }
---
+++ class/polymorphism.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+
+//Q8
+/*void print(string halo)
+{
+    cout<<halo<<endl;
+}
+
+void print(int num)
+{
+    cout<<num<<endl;
+}
+
+void print(int num, string halo)
+{
+    cout<<halo+" "<<num<<endl;
+}
+
+int main()
+{
+    print("halo");
+    print(123);
+    print(3,"halo");
+}*/
+
+//Q9
+/*int sum(int num1,int num2)
+{
+    return num1+num2;
+}
+
+double sum(double num1, double num2, double num3)
+{
+    return num1+num2+num3;
+}
+
+void sum(int num1,double num2)
+{
+    cout<<num1+num2;
+}
+
+int main()
+{
+    cout<<sum(1,2)<<endl;
+    cout<<sum(1,2,3)<<endl;
+    sum(1,2.0);
+}*/
+
+//Q10
+/*int multiply(int num1,int num2)
+{
+    return num1*num2;
+}
+
+void multiply(int num1,double num2)
+{
+    cout<< num1*num2<<endl;
+}
+
+float multiply(float num1,float num2,float num3)
+{
+    return num1*num2*num3;
+}
+
+int main()
+{
+    cout<<multiply(3,3)<<endl;
+    multiply(2,3.0);
+    cout<<multiply(2.0,3.0,4.0)<<endl;
+}*/
+
