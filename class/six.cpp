d684d407500a58bf4be3046b56546f9b94bf30b6
Ayush Kumar <ayushkj043@gmail.com>
2025-02-16 17:44:56 +0530

    the-five

---
+++ class/six.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+/*int main()
+
+{
+    int *ptr;
+    int var=1000;
+    ptr=&var;
+    cout<<&ptr<<endl;
+    cout<<&var<<endl;
+    cout<<var;
+}*/
+
+void swap(int *var1,int *var2);
+int main()
+{
+    
+
+}
+
+void swap(int *var1, int *var2)
+{
+
+}
